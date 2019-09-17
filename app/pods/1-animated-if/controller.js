bc33161437af6f142bcb5cc451fe12d3d8b50a20
William Chandler <chndlr.wllm@gmail.com>
2019-09-17 16:28:09 -0500

    custom transition, staggered animation

--- app/pods/1-animated-if/controller.js
+++ app/pods/1-animated-if/controller.js
@@ -1,8 +1,18 @@
 import Controller from '@ember/controller';
 import fade from 'ember-animated/transitions/fade';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 
 export default Controller.extend({
 
-  transition: fade
+  // transition: fade
 
+  transition: function*({ insertedSprites, removedSprites, duration }) {
+    for (let sprite of removedSprites) {
+      yield fadeOut(sprite, { duration: duration / 2 });
+    }
+
+    for (let sprite of insertedSprites) {
+      fadeIn(sprite, { duration: duration / 2 });
+    }
+  }
 });
