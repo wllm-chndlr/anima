ebe8a13f29e14bea8b117c96b445960761ad14cd
William Chandler <chndlr.wllm@gmail.com>
2019-10-01 20:32:03 -0500

    custom easing

--- app/pods/4-animated-value/controller.js
+++ app/pods/4-animated-value/controller.js
@@ -1,9 +1,7 @@
 import Controller from '@ember/controller';
 import faker from 'faker';
-import { toLeft, toRight } from 'ember-animated/transitions/move-over';
 import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 import move from 'ember-animated/motions/move';
-import { wait } from 'ember-animated';
 
 export default Controller.extend({
   *transition({ duration, insertedSprites, removedSprites }) {
