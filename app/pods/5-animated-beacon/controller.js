d84859b7360292fb5641eb94ea2504fb6d0f2ea7
William Chandler <chndlr.wllm@gmail.com>
2019-10-02 20:50:31 -0500

    animated beacon (start)

---
+++ app/pods/5-animated-beacon/controller.js
@@ -0,0 +1,54 @@
+import Controller from "@ember/controller";
+import { A } from "@ember/array";
+import move from "ember-animated/motions/move";
+import { fadeOut } from "ember-animated/motions/opacity";
+
+export default Controller.extend({
+  * transition({ keptSprites, removedSprites }) {
+
+    yield Promise.all(
+      removedSprites.map(sprite => {
+        return fadeOut(sprite);
+      })
+    );
+
+    for (let sprite of keptSprites) {
+      move(sprite);
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+
+    this.set("selectedItems", A([]));
+    this.set("deletedItems", A([]));
+
+    this.set(
+      "items",
+      A(
+        [...Array(124)].map((_, i) => ({
+          id: i,
+          name: i + 1
+        }))
+      )
+    );
+  },
+
+  actions: {
+    toggle(item) {
+      if (this.selectedItems.includes(item)) {
+        this.selectedItems.removeObject(item);
+      } else {
+        this.selectedItems.addObject(item);
+      }
+    },
+
+    deleteSelectedItems() {
+      this.selectedItems.toArray().forEach(item => {
+        this.items.removeObject(item);
+        this.deletedItems.pushObject(item);
+        this.selectedItems.removeObject(item);
+      });
+    }
+  }
+});
