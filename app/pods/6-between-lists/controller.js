79ff1824a942ffb6a8ccdc07db624de7c80b1e60
William Chandler <chndlr.wllm@gmail.com>
2019-10-08 20:41:50 -0500

    sent/rcvd part deux

--- app/pods/6-between-lists/controller.js
+++ app/pods/6-between-lists/controller.js
@@ -1,7 +1,7 @@
 import Controller from "@ember/controller";
 import { A } from "@ember/array";
 import move from "ember-animated/motions/move";
-import { fadeOut } from "ember-animated/motions/opacity";
+import { fadeIn, fadeOut } from "ember-animated/motions/opacity";
 
 export default Controller.extend({
 
@@ -10,13 +10,24 @@ export default Controller.extend({
 
     this.redList = A([1, 2, 3]);
     this.blueList = A([]);
-
   },
 
-  * redTransition({ removedSprites, keptSprites }) {
-      removedSprites.forEach(fadeOut);
-      keptSprites.forEach(move);
+  *redTransition({ insertedSprites, removedSprites, keptSprites, sentSprites, receivedSprites }) {
+    console.log(arguments[0]);
+
+    insertedSprites.forEach(fadeIn);
+    removedSprites.forEach(fadeOut);
+    keptSprites.forEach(move);
+
+    sentSprites.forEach(move);
+    receivedSprites.forEach(move);
   },
+
+  // *blueTransition({ insertedSprites, removedSprites, keptSprites }) {
+  //   insertedSprites.forEach(fadeIn);
+  //   removedSprites.forEach(fadeOut);
+  //   keptSprites.forEach(move);
+  // },
 
   actions: {
     moveToBlue(item) {
