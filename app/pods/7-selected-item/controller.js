a8954e4c5a715bdd1a85effaac800bd7a65f0133
William Chandler <chndlr.wllm@gmail.com>
2019-10-11 16:52:36 -0500

    multiple animators (cont)

--- app/pods/7-selected-item/controller.js
+++ app/pods/7-selected-item/controller.js
@@ -1,6 +1,7 @@
 import Controller from "@ember/controller";
 import { A } from "@ember/array";
 import move from "ember-animated/motions/move";
+import { fadeOut } from "ember-animated/motions/opacity";
 
 export default Controller.extend({
 
@@ -12,10 +13,27 @@ export default Controller.extend({
 
   selectedItem: null,
 
+  *redTransition({ removedSprites, keptSprites, sentSprites }) {
+    keptSprites.forEach(move);
+    removedSprites.forEach(fadeOut);
+    sentSprites.forEach(move);
+  },
+
+  *selectedTransition({ removedSprites, receivedSprites }) {
+    removedSprites.forEach(fadeOut);
+    receivedSprites.forEach(move);
+  },
+
   actions: {
     select(item) {
-      this.set('selectedItem', item);
-    }
+      // Add back in old item
+      if (this.selectedItem) {
+        this.redList.addObject(this.selectedItem);
+      }
 
+      // Set new item
+      this.set('selectedItem', item);
+      this.redList.removeObject(item);
+    }
   }
 });
