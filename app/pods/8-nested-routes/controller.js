7479cc4125651c2bcdd8ea87cfb5cae7154eac2f
William Chandler <chndlr.wllm@gmail.com>
2019-10-15 20:57:28 -0500

    movies part 1

---
+++ app/pods/8-nested-routes/controller.js
@@ -0,0 +1,11 @@
+import Controller from "@ember/controller";
+// import { A } from "@ember/array";
+// import move from "ember-animated/motions/move";
+// import { fadeOut } from "ember-animated/motions/opacity";
+// import { computed } from '@ember/object';
+
+export default Controller.extend({
+
+
+
+});
