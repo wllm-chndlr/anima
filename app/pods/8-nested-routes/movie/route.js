a7daa3285c6150f8edb2c234696c9f5023e51d9e
William Chandler <chndlr.wllm@gmail.com>
2019-10-17 13:56:54 -0500

    movies cont

---
+++ app/pods/8-nested-routes/movie/route.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+    model(params) {
+        return this.modelFor('8-nested-routes').findBy('id', params.movie_id);
+    }
+
+});
