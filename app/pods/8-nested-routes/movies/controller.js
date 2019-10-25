46a0a8440bd0b26aac2a558f0a923de40a95d658
William Chandler <chndlr.wllm@gmail.com>
2019-10-24 21:08:59 -0500

    animate on insertion (start)

--- app/pods/8-nested-routes/movies/controller.js
+++ app/pods/8-nested-routes/movies/controller.js
@@ -1,12 +1,15 @@
 import Controller from "@ember/controller";
 import move from "ember-animated/motions/move";
 import resize from "ember-animated/motions/resize";
+import { fadeIn } from "ember-animated/motions/opacity";
 
 export default Controller.extend({
-    *transition({ receivedSprites }) {
+    *transition({ receivedSprites, insertedSprites }) {
         receivedSprites.forEach(sprite => {
             move(sprite);
             resize(sprite);
         });
+
+        insertedSprites.forEach(sprite => fadeIn(sprite));
     }
 });
