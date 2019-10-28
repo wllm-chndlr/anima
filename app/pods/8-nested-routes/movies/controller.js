745009326c90a40594b1d254d16718164aeb954e
William Chandler <chndlr.wllm@gmail.com>
2019-10-28 16:58:03 -0500

    animating on insertion

--- app/pods/8-nested-routes/movies/controller.js
+++ app/pods/8-nested-routes/movies/controller.js
@@ -2,8 +2,11 @@ import Controller from "@ember/controller";
 import move from "ember-animated/motions/move";
 import resize from "ember-animated/motions/resize";
 import { fadeIn } from "ember-animated/motions/opacity";
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    appState: service(),
+
     *transition({ receivedSprites, insertedSprites }) {
         receivedSprites.forEach(sprite => {
             move(sprite);
---
+++ app/pods/ui-hooks/component.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    'did-insert'() {},
+
+    didInsertElement() {
+        this._super(...arguments);
+
+        this['did-insert']();
+    }
+
+
+})
