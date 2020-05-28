9ee5e28c30f352c0338d9f69ecc2ef72e5d4013c
William Chandler <chndlr.wllm@gmail.com>
2020-05-27 19:35:50 -0500

    removed animated value route pending module import fix

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
   this.route('1-animated-if');
   this.route('2-animated-each');
   this.route('3-iphone-apps');
-  this.route('4-animated-value');
+//   this.route('4-animated-value');
   this.route('5-animated-beacon');
   this.route('6-between-lists');
   this.route('7-selected-item');
